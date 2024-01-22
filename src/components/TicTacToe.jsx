9f3e86e9278f6b50529deaee0a7e4f0ec8e8d201
Michael Short <mshort0403@gmail.com>
2024-01-21 20:12:10 -0800

    Add tic-tac-toe

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,6 +1,63 @@
+import { useState } from 'react';
 import resetArrows from '../assets/resetArrows.svg';
 
+// define Square component
+function Square({ value, onSquareClick }) {
+	return (
+		<div
+			onClick={onSquareClick}
+			className='border border-neutral-50 p-4 hover:ring hover:ring-yellow-400'>
+			{value}
+		</div>
+	);
+}
+
 function TicTacToe() {
+	// set first player as ✖️
+	const [xIsNext, setXIsNext] = useState(true);
+	// create empty array in squares state variable
+	const [squares, setSquares] = useState(Array(9).fill(null));
+
+	// define event handler where i is the index of the array
+	function handleClick(i) {
+		// check if square is already filled or if there's a winner
+		if (squares[i] || calculateWinner(squares)) {
+			return;
+		}
+
+		// copy the original array
+		const nextSquares = squares.slice();
+
+		// change copied array
+		if (xIsNext) {
+			nextSquares[i] = '✖️';
+		} else {
+			nextSquares[i] = '⭕';
+		}
+
+		// change original array
+		setSquares(nextSquares);
+		// toggle next player with boolean
+		setXIsNext(!xIsNext);
+	}
+
+	const winner = calculateWinner(squares);
+	const draw = calculateDraw(squares);
+	let status;
+
+	if (winner) {
+		status = `${winner} wins!`;
+	} else if (draw) {
+		status = `It's a draw!`;
+	} else {
+		status = xIsNext ? '✖️ is next' : '⭕ is next';
+	}
+
+	function resetGame() {
+		setXIsNext(true);
+		setSquares(Array(9).fill(null));
+	}
+
 	return (
 		<>
 			<h1 className='text-neutral-50 text-xl text-center pt-32 absolute w-screen'>
@@ -9,47 +66,50 @@ function TicTacToe() {
 
 			<div className='flex justify-center items-center h-screen w-screen'>
 				<div className='flex-1 max-w-xs'>
-					<div
-						id='board'
-						className='grid grid-rows-3 grid-cols-3 h-60 border cursor-pointer text-center text-4xl mb-2'>
-						<div
-							id='cell-1'
-							className='border border-neutral-50 p-4 hover:ring hover:ring-yellow-400'></div>
-						<div
-							id='cell-2'
-							className='border border-neutral-50 p-4 hover:ring hover:ring-yellow-400'></div>
-						<div
-							id='cell-3'
-							className='border border-neutral-50 p-4 hover:ring hover:ring-yellow-400'></div>
-						<div
-							id='cell-4'
-							className='border border-neutral-50 p-4 hover:ring hover:ring-yellow-400'></div>
-						<div
-							id='cell-5'
-							className='border border-neutral-50 p-4 hover:ring hover:ring-yellow-400'></div>
-						<div
-							id='cell-6'
-							className='border border-neutral-50 p-4 hover:ring hover:ring-yellow-400'></div>
-						<div
-							id='cell-7'
-							className='border border-neutral-50 p-4 hover:ring hover:ring-yellow-400'></div>
-						<div
-							id='cell-8'
-							className='border border-neutral-50 p-4 hover:ring hover:ring-yellow-400'></div>
-						<div
-							id='cell-9'
-							className='border border-neutral-50 p-4 hover:ring hover:ring-yellow-400'></div>
+					<div className='grid grid-rows-3 grid-cols-3 h-60 border cursor-pointer text-center text-4xl mb-2'>
+						<Square
+							value={squares[0]}
+							onSquareClick={() => handleClick(0)}
+						/>
+						<Square
+							value={squares[1]}
+							onSquareClick={() => handleClick(1)}
+						/>
+						<Square
+							value={squares[2]}
+							onSquareClick={() => handleClick(2)}
+						/>
+						<Square
+							value={squares[3]}
+							onSquareClick={() => handleClick(3)}
+						/>
+						<Square
+							value={squares[4]}
+							onSquareClick={() => handleClick(4)}
+						/>
+						<Square
+							value={squares[5]}
+							onSquareClick={() => handleClick(5)}
+						/>
+						<Square
+							value={squares[6]}
+							onSquareClick={() => handleClick(6)}
+						/>
+						<Square
+							value={squares[7]}
+							onSquareClick={() => handleClick(7)}
+						/>
+						<Square
+							value={squares[8]}
+							onSquareClick={() => handleClick(8)}
+						/>
 					</div>
 
 					<div className='flex justify-between items-center'>
-						<div
-							id='tic-tac-toe'
-							className='text-neutral-50 text-xl'>
-							✖️ has won!
-						</div>
+						<div className='text-neutral-50 text-xl'>{status}</div>
 						<div>
 							<img
-								id='resetBtn'
+								onClick={resetGame}
 								src={resetArrows}
 								className='w-8 cursor-pointer filter invert'
 							/>
@@ -59,6 +119,39 @@ function TicTacToe() {
 			</div>
 		</>
 	);
+}
+
+// use indices to check for all possible winning combinations
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (
+			squares[a] &&
+			squares[a] === squares[b] &&
+			squares[a] === squares[c]
+		) {
+			return squares[a];
+		}
+	}
+	return null;
+}
+
+// check if every square is filled for a draw
+function calculateDraw(squares) {
+	if (squares.every((square) => square !== null)) {
+		return true;
+	}
+	return null;
 }
 
 export default TicTacToe;
