11136b2caed6ffca2dc4b916f964f54fe4ccefb2
Michael Short <mshort0403@gmail.com>
2024-01-24 11:11:46 -0800

    Add mini projects

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,21 +8,21 @@ function Home() {
 			<div className='flex justify-center items-center h-screen w-screen'>
 				<div className='flex-1 max-w-xs'>
 					<div className='flex justify-center'>
-						<div className='text-4xl border-2 border-neutral-50 rounded-lg p-4 m-2 hover:bg-black hover:shadow-md hover:shadow-yellow-400 selection:bg-transparent'>
+						<div className='cursor-pointer text-4xl border-2 border-neutral-50 rounded-lg p-4 m-2 hover:bg-black hover:shadow-md hover:shadow-yellow-400 selection:bg-transparent'>
 							✖️
 						</div>
-						<div className='text-4xl border-2 border-neutral-50 rounded-lg p-4 m-2 hover:bg-black hover:shadow-md hover:shadow-yellow-400 selection:bg-transparent'>
+						<div className='cursor-pointer text-4xl border-2 border-neutral-50 rounded-lg p-4 m-2 hover:bg-black hover:shadow-md hover:shadow-yellow-400 selection:bg-transparent'>
 							🌦️
 						</div>
-						<div className='text-4xl border-2 border-neutral-50 rounded-lg p-4 m-2 hover:bg-black hover:shadow-md hover:shadow-yellow-400 selection:bg-transparent'>
+						<div className='cursor-pointer text-4xl border-2 border-neutral-50 rounded-lg p-4 m-2 hover:bg-black hover:shadow-md hover:shadow-yellow-400 selection:bg-transparent'>
 							📝
 						</div>
 					</div>
 					<div className='flex justify-center'>
-						<div className='text-4xl border-2 border-neutral-50 rounded-lg p-4 m-2 hover:bg-black hover:shadow-md hover:shadow-yellow-400 selection:bg-transparent'>
+						<div className='cursor-pointer text-4xl border-2 border-neutral-50 rounded-lg p-4 m-2 hover:bg-black hover:shadow-md hover:shadow-yellow-400 selection:bg-transparent'>
 							💵
 						</div>
-						<div className='text-4xl border-2 border-neutral-50 rounded-lg p-4 m-2 hover:bg-black hover:shadow-md hover:shadow-yellow-400 selection:bg-transparent'>
+						<div className='cursor-pointer text-4xl border-2 border-neutral-50 rounded-lg p-4 m-2 hover:bg-black hover:shadow-md hover:shadow-yellow-400 selection:bg-transparent'>
 							🦒
 						</div>
 					</div>
